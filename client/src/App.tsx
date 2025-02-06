f195cb2041626e0f7e9db8a55c5b922ea19c9600
Amine <aminemannai13@gmail.com>
2025-02-06 01:32:14 +0100

    feat : adding tailwindcss

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 import "./App.css";
 
 function App() {
-  return <>Hello App</>;
+  return (
+    <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
+  );
 }
 
 export default App;
